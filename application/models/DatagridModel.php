d235ae859c2f938c665971273641a2ed8681c31d
Philippe Krejčí <kphil@protonmail.com>
2021-01-13 17:43:24 +0100

    Cleaning class DatagridModel

--- application/models/DatagridModel.php
+++ application/models/DatagridModel.php
@@ -4,50 +4,59 @@ use f3il\Error;
 
 class DatagridModel
 {
-    public static function getAll()
-    {
-        $r = "[";
-        $db = \f3il\Database::getInstance();
-        $req = $db->prepare("SELECT nom, prenom, cycle, promotion, groupe FROM eleves2");
-        try {
-            $req->execute();
-            $data = $req->fetchAll();
-            foreach($data as $k=>$d)
-            	$r = $r."[\"".$d['nom']."\",\"".$d['prenom']."\",\"".$d['cycle']."\",\"".$d['promotion']."\",\"".$d['groupe']."\"],";
-	    $r[strlen($r)-1]=' ';
-	    return $r."]"; 
-        } catch (\PDOException $ex) {
-            throw new Error("DatagridModel : erreur SQL{$ex->getMessage()}");
-        }
-    }
 
-    public static function getElevesFromPromo($prom)
-    {
-        $db = \f3il\Database::getInstance();
-        $req = $db->prepare("SELECT nom, prenom, cycle, promotion, groupe FROM eleves2 WHERE promotion=:promotion");
-        try {
-            $req->execute([':promotion'=>$prom]);
-            $data = $req->fetchAll();
-	    return DatagridModel::export($data,array('nom','prenom','cycle','promotion','groupe'));
-        } catch (\PDOException $ex) {
-            throw new Error("DatagridModel : erreur SQL{$ex->getMessage()}");
-        }
-    }
+	private static function req($demande,$option=NULL) {
+		$db = \f3il\Database::getInstance();
+		$req = $db->prepare($demande);
+		try {
+			if( $option == NULL )
+				$req->execute();
+			else
+				$req->execute($option);
 
-
-    public static function export($data,$tab)
-    {
-        $r = "[";
-	foreach($data as $k=>$d) {
-		$r=$r."[";
-		foreach($tab as $elem) 
-			$r = $r."\"".$d[$elem]."\",";
-		$r[strlen($r)-1]=' ';
-		$r = $r."],"; 
+			return $req->fetchAll();
+		} catch (\PDOException $ex) {
+			throw new Error("ReleveModel : erreur SQL{$ex->getMessage()}");
+		}
 	}
-	$r[strlen($r)-1]=']';
-	return $r;
-    }
+
+
+	public static function export($data,$tab) {
+		$r = "[";
+		foreach($data as $k=>$d) {
+			$r=$r."[";
+			foreach($tab as $elem) 
+				$r = $r."\"".$d[$elem]."\",";
+			$r[strlen($r)-1]=' ';
+			$r = $r."],"; 
+		}
+		$r[strlen($r)-1]=']';
+		return $r;
+	}
+
+
+	/* ------------------------------ */
+
+	public static function getAll() {
+        	$sql = "SELECT nom, prenom, cycle, promotion, groupe FROM eleves2";
+		return 	DatagridModel::export(
+				DatagridModel::req($sql),
+				array('nom','prenom','cycle','promotion','groupe')
+			);
+
+    	}
+
+	public static function getElevesFromPromo($prom) {
+	$sql = "SELECT nom, prenom, cycle, promotion, groupe FROM eleves2 WHERE promotion=:promotion";
+	return 	DatagridModel::export(
+			DatagridModel::req(
+				$sql,array(':promotion'=>$prom)),
+				array('nom','prenom','cycle','promotion','groupe')
+		);
+	}
+
+
+    
 
 
 }
